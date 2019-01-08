cd24a3626f49b76dc957dfd104541b277cfa8aaa
shuhui <793983383@qq.com>
2019-01-08 20:50:13 +0800

    ge'ge
    个个

--- YSHNetWorkTool/Classes/PPNetworkHelper/PPNetworkHelper.m
+++ YSHNetWorkTool/Classes/PPNetworkHelper/PPNetworkHelper.m
@@ -414,6 +414,7 @@ static AFHTTPSessionManager *_sessionManager;
     _sessionManager = [AFHTTPSessionManager manager];
     _sessionManager.requestSerializer.timeoutInterval = 30.f;
     _sessionManager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/html", @"text/json", @"text/plain", @"text/javascript", @"text/xml", @"image/*", nil];
+    [_sessionManager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     // 打开状态栏的等待菊花
     [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
 }
